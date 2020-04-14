ccbaafbfd30e8e5001d398d83ff2dc5fa25f9f1b
SwordYeah <59214112+SwordYeah@users.noreply.github.com>
2020-04-14 19:26:41 +0800

    补全了BoundedBuffer类的头文件和函数实现，并添加了注释

---
+++ Lab2/BoundedBuffer.cc
@@ -0,0 +1,125 @@
+#include "BoundedBuffer.h"
+#include "system.h"
+
+
+//----------------------------------------------------------------------
+// BoundedBuffer::BoundedBuffer
+//	    Initialize a BoundedBuffer.
+//----------------------------------------------------------------------
+BoundedBuffer::BoundedBuffer(int max_size)
+{
+    maxsize = max_size;
+    buffer = new int[max_size] ;
+    in = out = 0;
+    count = 0;
+    lock = new Lock("bufferLock");
+    WriteFull = new  Condition("WriteFull");
+    ReadEmpty = new Condition("ReadEmpty");
+    
+}
+
+
+//----------------------------------------------------------------------
+// BoundedBuffer::~BoundedBuffer
+//	   Destroy a BoundedBuffer.
+//----------------------------------------------------------------------
+BoundedBuffer::~BoundedBuffer()
+{
+    WriteFull->~Condition();
+    ReadEmpty->~Condition();
+    in = out = 0;
+}
+
+
+//----------------------------------------------------------------------
+//BoundedBuffer::IsEmpty
+//	Determine whether the buffer is empty
+//----------------------------------------------------------------------
+bool BoundedBuffer::IsEmpty()
+         {
+	return (count == 0);
+         }
+
+
+//----------------------------------------------------------------------
+//BoundedBuffer::IsFull
+//	Determine whether the buffer is full
+//----------------------------------------------------------------------
+bool BoundedBuffer::IsFull()
+         {
+	return (count == maxsize);
+         }
+
+
+//----------------------------------------------------------------------
+//BoundedBuffer::Read
+//	Read 'size' bytes from buffer and add them to '*data' .
+//      Read one character at a time into Space starting from *data 
+//      with locks and condition variables.
+//      'out' is the location of the first character in the current buffer
+//----------------------------------------------------------------------
+void  BoundedBuffer::Read(void *data,int size)
+{
+    int i;
+    int *s = (int *)data;
+    lock -> Acquire();
+    for(i =0;i<size;i++)
+        {
+	while(IsEmpty())
+		ReadEmpty -> Wait(lock);
+	*(s + i) = *(buffer +out);
+	out = (out+1)%maxsize;
+	count--;
+	WriteFull -> Signal(lock);
+        }
+    *(s + i) = '\0'; 
+    lock -> Release();
+}
+
+
+
+//----------------------------------------------------------------------
+//BoundedBuffer::Write
+//	Write 'size' bytes from '*data' to buffer,
+//      write one character at a time into the buffer 
+//      with locks and condition variables
+//       'in' is the location of the last character in the current buffer
+//----------------------------------------------------------------------
+void BoundedBuffer::Write(void *data,int size)
+{
+     int *s = (int *)data;
+     int i;
+     lock -> Acquire();
+     for(i=0;i<size;i++)
+        {
+	while(IsFull())
+		WriteFull -> Wait(lock);
+	*(buffer +in) = *(s + i);
+	in = (in+1)%maxsize;
+	count++;
+	ReadEmpty -> Signal(lock);
+        }
+    lock -> Release();
+}
+
+
+
+//----------------------------------------------------------------------
+//BoundedBuffer::PrintBuffer
+//	Print the contents of the current buffer.
+//      Just use for debugging.
+//----------------------------------------------------------------------
+void BoundedBuffer::PrintBuffer()
+{
+    int i = out;
+    int num =count;
+    printf("\nCurrent buffer is :");
+    while(num--)
+    {
+	printf("%d ",*(buffer+i));
+	i = (i + 1)%maxsize;
+    }
+    printf("\n");
+}
+
+
--- Lab2/BoundedBuffer.h
+++ Lab2/BoundedBuffer.h
@@ -1,8 +1,14 @@
+#include "synch.h"
+
 class BoundedBuffer {
    public:
      // create a bounded buffer with a limit of 'maxsize' bytes
-     BoundedBuffer(int maxsize);
-     
+     BoundedBuffer(int max_size);
+
+     //De-allocate BoundedBuffer, when no longer needed.  Assume no        
+     //one is still waiting on the semaphore!
+     ~BoundedBuffer();
+
      // read 'size' bytes from the bounded buffer, storing into 'data'.
      // ('size' may be greater than 'maxsize')
      void Read(void *data, int size);
@@ -10,7 +16,24 @@ class BoundedBuffer {
      // write 'size' bytes from 'data' into the bounded buffer.
      // ('size' may be greater than 'maxsize')
      void Write(void *data, int size);
+
+     //Determine whether the buffer is empty
+     bool IsEmpty();
+
+     //Determine whether the buffer is full
+     bool IsFull();
+
+     //Use for debug,print  the contents of the buffer
+     void PrintBuffer(); 
+
+
    private:
-     // ???
+     int   in,out,maxsize; 
+     int  *buffer;
+     int count;//Record the number of numbers in the buffer
+
+     Lock  *lock;
+     Condition *WriteFull, *ReadEmpty; 
+     
 };
 
