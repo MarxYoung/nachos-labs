3faf90f32b348d82efc626428d250f61b66dda4c
MarxYoung <690681697@qq.com>
2020-05-11 20:12:03 +0800

    书写基本框架

---
+++ Lab3/EventBarrier.cc
@@ -0,0 +1,60 @@
+// EventBarrier.cc 
+//
+// Copyright (c) 2020 MarxYoung.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "EventBarrier.h"
+
+//----------------------------------------------------------------------
+// EventBarrier::EventBarrier
+//----------------------------------------------------------------------
+
+EventBarrier::EventBarrier()
+{
+}
+
+//----------------------------------------------------------------------
+// EventBarrier::~EventBarrier
+//----------------------------------------------------------------------
+
+EventBarrier::~EventBarrier()
+{
+}
+
+//----------------------------------------------------------------------
+// EventBarrier::EventBarrier
+//----------------------------------------------------------------------
+
+void
+EventBarrier::Wait()
+{
+}
+
+//----------------------------------------------------------------------
+// EventBarrier::EventBarrier
+//----------------------------------------------------------------------
+
+void
+EventBarrier::Signal()
+{
+}
+
+//----------------------------------------------------------------------
+// EventBarrier::EventBarrier
+//----------------------------------------------------------------------
+
+void
+EventBarrier::Complete()
+{
+}
+
+//----------------------------------------------------------------------
+// EventBarrier::EventBarrier
+//----------------------------------------------------------------------
+
+int
+EventBarrier::Waiters()
+{
+}
